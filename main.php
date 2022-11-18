c3e9146a333ded48086782f019bfb778f1e9f4fd
Marcos Paiva <marcospaivajr7@gmail.com>
2022-11-17 21:46:42 -0300

    separacao do codigo em arquivos php

---
+++ main.php
@@ -0,0 +1,69 @@
+<main role="main" class="container">
+    <h1 class="text-center display-4" style="margin: 100px">EXEMPLO DE CANVAS DE USABILIDADE</h1>
+    <img class="canvas_img rounded mx-auto d-block img-responsive img-rounded" src="./assets/img/canvas.png" alt="canvas">
+    <a href="./assets/files/ocanvas.docx" download>
+    <div class="text-center">
+        <button type="button" class="btn btn-success text-center" style="margin: 10px auto">Baixar</button>
+    </div>
+    </a>
+    <h2 class="text-center display-3" style="margin: 50px">ESTUDOS DE CASO</h2>
+    <div class="cards">
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" style="width: 5rem" src="./assets/img/olx.jpg" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">Olx</h5>
+        <p class="card-text">Com o investimento nas ferramentas do LinkedIn, a OLX passou a contar...</p>
+        <a href="./assets/files/caso_olx.pdf" class="btn btn-primary" download>Baixar</a>
+        </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" style="width: 5rem" src="./assets/img/autopass.jpg" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">Autopass</h5>
+        <p class="card-text">Empresa de tecnologia, meios de pagamento e soluções para mobilidade urbana...</p>
+        <a href="./assets/files/caso_autopass.pdf" class="btn btn-primary" download>Baixar</a>
+        </div>
+    </div>
+    </div>
+
+    <div class="container" style="margin-top: 200px">
+    <!-- Example row of columns -->
+    <h2 class="display-4 text-center">Como usar o Canvas de Usabilidade?</h2>
+    <div class="row" style="margin-top: 50px">
+        <div class="col-md-4">
+        <h2>1. Defina a Solução</h2>
+        <p>O que será avaliado? </p>
+        </div>
+        <div class="col-md-4">
+        <h2>2. Defina os Requisitos</h2>
+        <p>Qual requisito ou funcionalidade será avaliado? </p>
+        </div>
+        <div class="col-md-4">
+        <h2>3. Defina os Usuários</h2>
+        <p>Quem são os participantes da avaliação?</p>
+        </div>
+        <div class="col-md-4">
+        <h2>4. Defina a(s) Tarefas</h2>
+        <p>Quais tarefas os participantes irão realizar?</p>
+        </div>
+        <div class="col-md-4">
+        <h2>5. Defina o Contexto</h2>
+        <p>Em quais contextos estão inseridos?</p>
+        </div>
+        <div class="col-md-4">
+        <h2>6. Defina a(s) Alternativas</h2>
+        <p>Quais alternativas serão utilizadas?</p>
+        </div>
+        <div class="col-md-4">
+        <h2>7. Defina os Critérios</h2>
+        <p>Quais são os critérios utilizados?</p>
+        </div>
+        <div class="col-md-4">
+        <h2>8. Defina as Métricas</h2>
+        <p>Quais são as métricas utilizados?</p>
+        </div>
+    </div>
+    <hr>
+    </div> <!-- /container -->
+</main><!-- /.container -->
---
+++ section.php
@@ -0,0 +1,29 @@
+<section class="newsletter" style="margin-top: 200px">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="content">
+              <h2>INSCREVA-SE NA NOSSA NEWSLETTER</h2>
+            <div class="input-group">
+              <input type="email" class="form-control" placeholder="Insira seu e-mail">
+              <span class="input-group-btn">
+              <button class="btn" type="submit">Inscreva-se agora!</button>
+              </span>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+</section>
+
+<footer class="blog-footer" style="margin-top: 200px">
+    <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+    <p>
+    Voltar ao topo:
+    <a href="#">
+        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+        </svg>
+    </a>
+    </p>
+</footer>
