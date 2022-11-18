7f093eee3980bfee938258afc75080f7e476c6fa
Marcos Paiva <marcospaivajr7@gmail.com>
2022-11-17 21:53:06 -0300

    adicionado botao do whatsapp

--- index.php
+++ index.php
@@ -21,11 +21,13 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <!-- css local -->  
     <link rel="stylesheet" href="./assets/css/style.css">
   </head>
   
   <body>
+    <?php include 'whatsapp_btn.php'; ?>
     <?php include 'header.php'; ?>
     <?php include 'main.php'; ?>
     <?php include 'section.php'; ?>
---
+++ whatsapp_btn.php
@@ -0,0 +1,3 @@
+<a href="#" class="float" target="_blank">
+    <i class="fa fa-whatsapp my-float"></i>
+</a>
