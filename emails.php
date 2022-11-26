c11ee9d695a10bd3c04e034e2d2a7cc803629380
Marcos Paiva <marcospaivajr7@gmail.com>
2022-11-26 11:09:21 -0300

    CRUD completo

--- emails.php
+++ emails.php
@@ -22,7 +22,7 @@ if($result = mysqli_query($link, $sql)){
                     echo "<td>" . $row['nome'] . "</td>";
                     echo "<td>" . $row['email'] . "</td>";
                     echo "<td>";
-                        echo '<a href="read.php?id='. $row['id'] .'" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                        // echo '<a href="read.php?id='. $row['id'] .'" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
                         echo '<a href="update.php?id='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
                         echo '<a href="delete.php?id='. $row['id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
                     echo "</td>";
